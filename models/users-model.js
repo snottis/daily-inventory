40c0967cce12cdceba15f052125ab97151bdd3a0
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-02-21 00:44:06 +0200

    Add user-model functions

--- models/users-model.js
+++ models/users-model.js
@@ -1,5 +1,12 @@
 const client = require("../db");
 
+/**
+ * Connect to database and add user
+ * @param {string} username
+ * @param {string} password
+ * @param {number[]} locations
+ * @param {string} role
+ */
 exports.createUser = async (username, password, locations, role = "user") => {
   const db = client.db(process.env.DB_NAME);
   const r = await db.collection("users").insertOne({
@@ -12,13 +19,28 @@ exports.createUser = async (username, password, locations, role = "user") => {
 };
 
 exports.deleteUser = async (username) => {
-  // const r = await db.users.remove
+  const db = client.db(process.env.DB_NAME);
+  const r = await db.collection("users").deleteOne({ username });
+  return r;
 };
 
-exports.updateUser = async (username, password, locations, role) => {};
+exports.updateUser = async (username, password, locations, role) => {
+  const db = client.db(process.env.DB_NAME);
+  const r = await db
+    .collection("users")
+    .updateOne({ username }, { password, locations, role });
+  return r;
+};
 
-exports.getOneUser = async (username, query = {}) => {};
+exports.getOneUser = async (username, query = {}) => {
+  const db = client.db(process.env.DB_NAME);
+  const r = await db.collection("users").findOne(username);
+  return r;
+};
 
+/**
+ * Connect to database and get all users
+ */
 exports.getAllUsers = async () => {
   const db = client.db(process.env.DB_NAME);
   const r = await db.collection("users").find({}).toArray();
