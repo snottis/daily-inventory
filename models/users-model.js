6e11a15042badb7fb392b7cbf9bcdbcdcdff2047
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-02-21 09:05:13 +0200

    Add comments to user-model functions

--- models/users-model.js
+++ models/users-model.js
@@ -18,12 +18,23 @@ exports.createUser = async (username, password, locations, role = "user") => {
   return r;
 };
 
+/**
+ * Connect to database and delete user
+ * @param {string} username
+ */
 exports.deleteUser = async (username) => {
   const db = client.db(process.env.DB_NAME);
   const r = await db.collection("users").deleteOne({ username });
   return r;
 };
 
+/**
+ *
+ * @param {string} username
+ * @param {string} password
+ * @param {number[]} locations
+ * @param {string} role
+ */
 exports.updateUser = async (username, password, locations, role) => {
   const db = client.db(process.env.DB_NAME);
   const r = await db
