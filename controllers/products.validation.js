be3b2cee7c20a6d8586a8fa6ea409b0649cdafad
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-02-26 17:28:49 +0200

    Add products validator

---
+++ controllers/products.validation.js
@@ -0,0 +1,10 @@
+const { number } = require("joi");
+const Joi = require("joi");
+
+exports.product = Joi.object({
+  gtin: Joi.string().alphanum().required(),
+  name: Joi.string().required(),
+  value: Joi.number().required(),
+  lotsize: Joi.number().required(),
+  hidden: Joi.boolean(),
+});
