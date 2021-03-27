d31f3ffcb512f4a7560fa7e77ef6343548fee948
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-03-27 22:02:13 +0200

    Finalize product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -7,3 +7,14 @@ const productSchema = new mongoose.Schema({
   lotsize: { type: Number, required: true },
   hidden: { type: Boolean, required: true, default: false },
 });
+
+productSchema.set('toJSON', {
+  transform: (document: any, returnedObject: any) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
